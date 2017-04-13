8a89fdd45999364370eef627130f8e545cc21f09
mr.debbie <giyoungchong1980@gmail.com>
2017-04-12 21:27:55 -0500

    add api controllers

--- app_api/controllers/users.js
+++ app_api/controllers/users.js
@@ -29,7 +29,7 @@ module.exports.userReadOne = function(req, res) {
             .exec(function(err, profile) {
                 if (!profile) {
                     sendJSONresponse(res, 404, {
-                        "message": "userid not found"
+                        "message": "user id not found"
                     });
                     return;
                 } else if (err) {
@@ -41,9 +41,9 @@ module.exports.userReadOne = function(req, res) {
                 sendJSONresponse(res, 200, profile);
             });
     } else {
-        console.log('No userid specified');
+        console.log('No user id specified');
         sendJSONresponse(res, 404, {
-            "message": "No userid in request"
+            "message": "No user id in request"
         });
     }
 };
@@ -51,20 +51,96 @@ module.exports.userReadOne = function(req, res) {
     res.status(200);
     res.json({"status" : "success"});
 };*/
+/* POST a new profile */
+/* /api/profiles */
+module.exports.createUser = function(req, res) {
+    console.log(req.body);
+    Profile.create({
+        name: req.body.name,
+        password: req.body.password
+    }, function(err, profile) {
+        if (err) {
+            console.log(err);
+            sendJSONresponse(res, 400, err);
+        } else {
+            console.log(profile);
+            sendJSONresponse(res, 201, profile);
+        }
+    });
+};
 
-module.exports.createUser = function (req, res) {
+/*module.exports.createUser = function (req, res) {
     res.status(200);
     res.json({"status" : "success"});
-};
+};*/
 /*module.exports.userReadOne = function (req, res) {
     res.status(200);
     res.json({"status" : "success"});
 };*/
-module.exports.userUpdateOne = function (req, res) {
+/* PUT /api/profiles/:profileid */
+
+module.exports.userUpdateOne = function(req, res) {
+    if (!req.params.userid) {
+        sendJSONresponse(res, 404, {
+            "message": "Not found, user id is required"
+        });
+        return;
+    }
+    Profile
+        .findById(req.params.userid)
+        .select('name password')
+        .exec(
+            function(err, profile) {
+                if (!profile) {
+                    sendJSONresponse(res, 404, {
+                        "message": "user id not found"
+                    });
+                    return;
+                } else if (err) {
+                    sendJSONresponse(res, 400, err);
+                    return;
+                }
+                profile.name = req.body.name;
+                profile.password = req.body.password;
+                profile.save(function(err, profile) {
+                    if (err) {
+                        sendJSONresponse(res, 404, err);
+                    } else {
+                        sendJSONresponse(res, 200, profile);
+                    }
+                });
+            }
+        );
+};
+/*module.exports.userUpdateOne = function (req, res) {
     res.status(200);
     res.json({"status" : "success"});
+};*/
+
+/* DELETE /api/profiles/:profileid */
+module.exports.userDeleteOne = function(req, res) {
+    var userid = req.params.userid;
+    if (userid) {
+        Profile
+            .findByIdAndRemove(userid)
+            .exec(
+                function(err, profile) {
+                    if (err) {
+                        console.log(err);
+                        sendJSONresponse(res, 404, err);
+                        return;
+                    }
+                    console.log("profile id " + userid + " deleted");
+                    sendJSONresponse(res, 204, null);
+                }
+            );
+    } else {
+        sendJSONresponse(res, 404, {
+            "message": "No  user id"
+        });
+    }
 };
-module.exports.userDeleteOne = function (req, res) {
+/*module.exports.userDeleteOne = function (req, res) {
     res.status(200);
     res.json({"status" : "success"});
-};
+};*/
