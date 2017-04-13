a9bbe02813170f244f66896b39ec83e68699d65f
mr.debbie <giyoungchong1980@gmail.com>
2017-04-12 21:19:55 -0500

    update api/GETS

--- app_api/controllers/users.js
+++ app_api/controllers/users.js
@@ -1,19 +1,65 @@
 var mongoose = require('mongoose');
-var Loc = mongoose.model('Profile');
+var Profile = mongoose.model('Profile');
 
+var sendJSONresponse = function(res, status, content) {
+    res.status(status);
+    res.json(content);
+};
 
-module.exports.displayUsers = function (req, res) {
+/* GET list of users */
+module.exports.displayUsers = function(req, res) {
+    Profile
+        .find()
+        .exec(
+            function(err, results) {
+                if (err) {
+                    console.log('results error:', err);
+                    sendJSONresponse(res, 404, err);
+                } else {
+                    sendJSONresponse(res, 200, results);
+                }
+            });
+};
+/* GET a profile by the id */
+module.exports.userReadOne = function(req, res) {
+    console.log('Finding profile details', req.params);
+    if (req.params && req.params.userid) {
+        Profile
+            .findById(req.params.userid)
+            .exec(function(err, profile) {
+                if (!profile) {
+                    sendJSONresponse(res, 404, {
+                        "message": "userid not found"
+                    });
+                    return;
+                } else if (err) {
+                    console.log(err);
+                    sendJSONresponse(res, 404, err);
+                    return;
+                }
+                console.log(profile);
+                sendJSONresponse(res, 200, profile);
+            });
+    } else {
+        console.log('No userid specified');
+        sendJSONresponse(res, 404, {
+            "message": "No userid in request"
+        });
+    }
+};
+/*module.exports.displayUsers = function (req, res) {
     res.status(200);
     res.json({"status" : "success"});
-};
+};*/
+
 module.exports.createUser = function (req, res) {
     res.status(200);
     res.json({"status" : "success"});
 };
-module.exports.userReadOne = function (req, res) {
+/*module.exports.userReadOne = function (req, res) {
     res.status(200);
     res.json({"status" : "success"});
-};
+};*/
 module.exports.userUpdateOne = function (req, res) {
     res.status(200);
     res.json({"status" : "success"});
