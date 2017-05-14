d5d430f0d7e107e1d88f6e4affa77b19638465ef
mr.debbie <giyoungchong1980@gmail.com>
2017-05-14 15:41:54 -0500

    adds routes for individual products/views

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,33 +2,49 @@ var express = require('express');
 var router = express.Router();
 var ctrlMain = require('../controllers/main');
 var ctrlAbout = require('../controllers/about');
-var ctrlShop = require('../controllers/shopCtrl');
-var ctrlRunway = require('../controllers/runCtrl');
-var ctrlMini = require('../controllers/miniCtrl');
-var ctrlAccount = require('../controllers/accountCtrl');
+/* sample 1 controllers */
+var ctrlShop = require('../controllers/sample1controllers/shopCtrl');
+var ctrlRunway = require('../controllers/sample1controllers/runCtrl');
+var ctrlMini = require('../controllers/sample1controllers/miniCtrl');
+var ctrlAccount = require('../controllers/sample1controllers/accountCtrl');
+var ctrlStuffs = require('../controllers/sample1controllers/stuffCtrl');
 
-
-
-/* GET home page. */
+/*GET main home page. */
 router.get('/', ctrlMain.index);
+
+/* GET sample1 landing page. */
+router.get('/sample1', ctrlMain.sample1);
+/* GET sample1 landing page. */
+router.get('/sample2', ctrlMain.sample2);
+/* GET sample1 landing page. */
+router.get('/sample3', ctrlMain.sample3);
+
+/* routes for sample 1 */
+/* GET stuff page. */
+router.get('/stuff/yang', ctrlStuffs.stuff);
+/* GET stuff page. */
+router.get('/stuff/moreyang', ctrlStuffs.stuff2);
 
 /* GET account page. */
 router.get('/account', ctrlAccount.account);
 /* GET account page. */
 router.get('/account/login', ctrlAccount.login);
+router.post('/account/login', ctrlAccount.createlogin);
 /* GET account page. */
 router.get('/account/register', ctrlAccount.register);
-
+/* GET wishlist page. */
+router.get('/account/wishlist', ctrlAccount.wishlist);
 
 /* GET shop page. */
 router.get('/shop', ctrlShop.shop);
 /* GET shop mom. */
 router.get('/shop/mom', ctrlShop.shopMom);
-/* GET shop mom. */
+/* GET shop mom alt skirt */
+router.get('/shop/mom/alt', ctrlShop.shopAlt);
+/* GET shop best friend */
 router.get('/shop/bf', ctrlShop.shopBf);
-/* GET shop mom. */
+/* GET shop you */
 router.get('/shop/you', ctrlShop.shopYou);
-
 
 /* GET runway page. */
 router.get('/runway', ctrlRunway.runway);
@@ -43,9 +59,9 @@ router.get('/runway/trend', ctrlRunway.runwayTrend);
 router.get('/mini', ctrlMini.mini);
 /* GET mini bag edit page. */
 router.get('/mini/bags', ctrlMini.miniBags);
-/* GET mini edit page. */
+/* GET mini shoe edit page. */
 router.get('/mini/shoes', ctrlMini.miniShoes);
-/* GET mini edit page. */
+/* GET mini jewels edit page. */
 router.get('/mini/jewels', ctrlMini.miniJewels);
 
 /* GET about page. */
